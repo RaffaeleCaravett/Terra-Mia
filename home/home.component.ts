9d70ec0652375f39e70458c907ad21e175028336
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-21 23:11:51 +0100

    created project and structure

---
+++ home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+images:any[]=
+[
+  {title:"../../../assets/Background-images/home/container/casc.jpg"},
+  {title:"../../../assets/Background-images/home/container/teglia.jpg"},
+  {title:"../../../assets/Background-images/home/container/cascetta.jpg"},
+ {title:"../../../assets/Background-images/home/container/fritti.jpg}"},
+  {title:"../../../assets/Background-images/home/container/margherita.jpg"},
+ {title: "../../../assets/Background-images/home/container/mortadella.jpg"},
+
+]
+}
---
+++ piero-amato/piero-amato.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piero-amato',
+  templateUrl: './piero-amato.component.html',
+  styleUrls: ['./piero-amato.component.scss']
+})
+export class PieroAmatoComponent {
+
+}
