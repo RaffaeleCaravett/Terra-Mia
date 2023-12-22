bb3158647151cee00a513ef1545be12f2c501403
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-22 15:42:19 +0100

    piero amato deploy

--- src/app/Components/piero-amato/piero-amato.component.ts
+++ src/app/Components/piero-amato/piero-amato.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-piero-amato',
   templateUrl: './piero-amato.component.html',
   styleUrls: ['./piero-amato.component.scss']
 })
-export class PieroAmatoComponent {
+export class PieroAmatoComponent implements OnInit{
+  ngOnInit(): void {
+Aos.init()
+ }
 
 }
